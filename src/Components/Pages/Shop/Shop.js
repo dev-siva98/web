ecd77505f46c4226ac7ddd6dc3bcbd2023b33b78
Siva <sivaprasadtppba@gmail.com>
2021-11-23 17:47:07 +0530

    shop-edit

--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Shop.css'
+import Select from 'react-select'
 
 function Shop() {
+
+
+    const options = [
+        { value: '500 G', label: '500 G' },
+        { value: '1 KG', label: '1 KG' },
+        { value: '2 KG', label: '2 KG' },
+    ];
+
+    const [select, setSelect] = useState(options[1].value)
+
+    const handleChange = (event) => {
+        setSelect(event.value)
+    }
+
+    console.log(select);
+
     return (
         <div>
             <div className="shop-section">
@@ -17,6 +34,36 @@ function Shop() {
                                     <h2 className="shop-item-name">
                                         Red Velvet
                                     </h2>
+                                    <div className="shop-select-container">
+                                        <Select
+                                            value={select}
+                                            onChange={handleChange}
+                                            options={options}
+                                            placeholder={select}
+                                            className='shop-select'
+                                        />
+                                    </div>
+                                    <h2>&#8377;600</h2>
+                                    <button className='btn btn-shop'>Add to Cart</button>
+                                </div>
+                            </div>
+                            <div className="shop-item">
+                                <div className="shop-item-image">
+                                    <img className='shop-item-img' src="images/custom.jpg" alt="cake" />
+                                </div>
+                                <div className="shop-item-content">
+                                    <h2 className="shop-item-name">
+                                        Red Velvet
+                                    </h2>
+                                    <div className="shop-select-container">
+                                        <Select
+                                            value={select}
+                                            onChange={handleChange}
+                                            options={options}
+                                            placeholder={select}
+                                            className='shop-select'
+                                        />
+                                    </div>
                                     <h2>&#8377;600</h2>
                                     <button className='btn btn-shop'>Customize</button>
                                 </div>
@@ -29,18 +76,15 @@ function Shop() {
                                     <h2 className="shop-item-name">
                                         Red Velvet
                                     </h2>
-                                    <h2>&#8377;600</h2>
-                                    <button className='btn btn-shop'>Customize</button>
-                                </div>
-                            </div>
-                            <div className="shop-item">
-                                <div className="shop-item-image">
-                                    <img className='shop-item-img' src="images/custom.jpg" alt="cake" />
-                                </div>
-                                <div className="shop-item-content">
-                                    <h2 className="shop-item-name">
-                                        Red Velvet
-                                    </h2>
+                                    <div className="shop-select-container">
+                                        <Select
+                                            value={select}
+                                            onChange={handleChange}
+                                            options={options}
+                                            placeholder={select}
+                                            className='shop-select'
+                                        />
+                                    </div>
                                     <h2>&#8377;600</h2>
                                     <button className='btn btn-shop'>Customize</button>
                                 </div>

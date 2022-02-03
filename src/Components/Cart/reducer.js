3dc37144fc71d9de772e75bf882512c59f1ee9ba
dev-siva98 <sivaprasadtpba@gmail.com>
2022-02-03 18:01:35 +0530

    basket logic

--- src/Components/Cart/reducer.js
+++ src/Components/Cart/reducer.js
@@ -6,13 +6,29 @@ function reducer(state, action) {
     console.log(action)
     switch (action.type) {
         case 'ADD_ITEM':
-            //add item
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
+            var flag = false
+            state.basket.map((product) => {
+                if (product.id === action.item.id) {
+                    flag = true
+                }
+            })
+            if (flag) {
+                alert('Item already added to the cart')
+                return state
+            } else {
+                return {
+                    ...state,
+                    basket: [...state.basket, action.item]
+                }
             }
         case 'REMOVE_ITEM':
-            //remove item
+            state.basket.map((product)=> {
+                if(product.id===action.item.id) {
+                    console.log('deleted')
+                }else {
+                    state.basket.push(product)
+                }
+            })
             return { state }
         default:
             return state;

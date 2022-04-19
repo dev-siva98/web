2c7485bb4c95d955afe710ee630aa3dc88c96e68
dev-siva98 <sivaprasadtpba@gmail.com>
2022-04-20 03:27:25 +0530

    form-validation

--- src/Components/Pages/Checkout/Checkout.js
+++ src/Components/Pages/Checkout/Checkout.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import axios from '../../../axios';
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { FaShippingFast } from 'react-icons/fa'
 import { GoVerified } from 'react-icons/go'
 import './Checkout.css'
@@ -22,7 +26,7 @@ function Checkout() {
         }
     }
 
-    const handleGetOtp = () =>{
+    const handleGetOtp = () => {
         setDone(false)
         setLoader(false)
         setVerify(null)
@@ -38,7 +42,37 @@ function Checkout() {
         }
     }, [verify])
 
-    console.log(verify)
+    const formSchema = Yup.object().shape({
+        address1: Yup.string()
+            .required('Missing Fields'),
+        contact: Yup.string()
+            .required('Missing Fields')
+            .length(10, 'Enter valid phone number'),
+        pincode: Yup.string()
+            .required('Missing Fields')
+            .min(6, 'Password length should be at least 4 characters'),
+        payment: Yup.string()
+            .required('Missing Fields'),
+        mobile: Yup.string()
+            .when('payment', {
+                is: 'cod',
+                then: Yup.string().required('Required')
+            })
+
+    })
+
+    const validationOpt = { resolver: yupResolver(formSchema) }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm(validationOpt);
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <div className="checkout-section">
             <div className="checkout-section-container">
@@ -60,26 +94,32 @@ function Checkout() {
                                     <div className="checkout-second-text">
                                         <h4 >Delivery Details</h4>
                                     </div>
-                                    <form className="checkout-address-form">
+                                    <form onSubmit={handleSubmit(onSubmit)} className="checkout-address-form">
                                         <div className="checkout-form-section-full">
-                                            <input type="text" className="checkout-form-input" placeholder='Write on cake' />
+                                            <input type="text" placeholder='Write on cake' {...register('write')}
+                                                className="checkout-form-input" />
                                         </div>
                                         <h3 className='checkout-form-section-headers'>Address</h3>
                                         <div className="checkout-form-section-full">
-                                            <input type="text" className="checkout-form-input" placeholder='Address Line 1' />
+                                            <input type="text" placeholder='Address Line 1' {...register('address1', { required: true })}
+                                                className={`checkout-form-input ${errors.address1 && 'checkout-input-error'}`} />
                                         </div>
                                         <div className="checkout-form-section-full">
-                                            <input type="text" className="checkout-form-input" placeholder='Address Line 2' />
+                                            <input type="text" placeholder='Address Line 2' {...register('address2')}
+                                                className="checkout-form-input" />
                                         </div>
                                         <div className="checkout-form-section-full">
-                                            <input type="text" className="checkout-form-input" placeholder='Landmark' />
+                                            <input type="text" placeholder='Landmark'  {...register('landmark')}
+                                                className="checkout-form-input" />
                                         </div>
                                         <div className="checkout-form-section">
                                             <div className="checkout-form-section-1">
-                                                <input type="tel" className="checkout-form-input checkout-form-input-contact" placeholder='Contact Number' />
+                                                <input type="tel" placeholder='Contact Number'  {...register('contact', { required: true })}
+                                                    className={`checkout-form-input checkout-form-input-contact ${errors.contact && 'checkout-input-error'}`} />
                                             </div>
                                             <div className="checkout-form-section-2">
-                                                <input type="zipcode" className="checkout-form-input checkout-form-input-small" placeholder='PIN code' />
+                                                <input type="zipcode" placeholder='PIN code'  {...register('pincode', { required: true })}
+                                                    className={`checkout-form-input checkout-form-input-small ${errors.pincode && 'checkout-input-error'}`} />
                                             </div>
                                         </div>
                                         <div className="checkout-form-payment-mode">
@@ -88,23 +128,25 @@ function Checkout() {
                                                 <div className='checkout-form-payment-section'>
                                                     <label type='button' className={`btn btn-checkout-payment-mode pay-online ${mode ? 'active' : ''}`}
                                                         onClick={() => setMode(true)} > Pay now Online
-                                                        <input type="radio" name="payment" value="online" defaultChecked />
+                                                        <input type="radio" name="payment" value="online" defaultChecked {...register('payment', { required: true })} />
                                                     </label>
                                                 </div>
                                                 <div className='checkout-form-payment-section' >
                                                     <label type='button' className={`btn btn-checkout-payment-mode ${mode ? '' : 'active'}`}
                                                         onClick={() => setMode(false)} >Cash on Delivery
-                                                        <input type="radio" name="payment" value="cod" />
+                                                        <input type="radio" name="payment" value="cod" {...register('payment', { required: true })} />
                                                     </label>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="checkout-form-section">
                                             <div className="checkout-form-section-1">
-                                                <input disabled={mode || loader || done} type="tel" className="checkout-form-input checkout-form-input-mobile" placeholder='Mobile' />
+                                                <input disabled={mode || loader || done} type="tel"
+                                                    className={`checkout-form-input checkout-form-input-mobile ${errors.mobile && 'checkout-input-error'}`}
+                                                    placeholder='Mobile'  {...register('mobile')} />
                                             </div>
                                             <button disabled={mode || loader || done} type='button' className="btn btn-checkout-mobile-verification"
-                                            onClick={() => handleGetOtp()}>Get OTP</button>
+                                                onClick={() => handleGetOtp()}>Get OTP</button>
                                             <div className="checkout-form-section-2">
                                                 <input disabled={mode || loader || done} type="otp" className={`checkout-form-input checkout-form-input-small ${done ? 'verified' : ''}`}
                                                     placeholder='OTP'

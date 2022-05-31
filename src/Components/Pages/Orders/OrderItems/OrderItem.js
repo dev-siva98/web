affaab7ff215458ba877e25a68937fd27b5ad82d
dev-siva98 <sivaprasadtppba@gmail.com>
2022-05-31 21:23:44 +0530

    orders basic setup

--- src/Components/Pages/Orders/OrderItems/OrderItem.js
+++ src/Components/Pages/Orders/OrderItems/OrderItem.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './OrderItem.css'
 
-function OrderItem() {
+function OrderItem({ order }) {
+  const createdAt = new Date(order.createdAt).toLocaleString('en-UK', { dateStyle: 'medium', timeStyle: 'short' })
+  const delivery = new Date(order.delivery).toLocaleDateString('en-IN', { dateStyle: 'medium' })
   return (
     <div className='order-item-section'>
       <div className="order-item-container">
         <div className="order-item-section-1">
-          <h4>Order Id : <span>MCPP1656789927</span></h4>
-          <h4>Order Date : <span>29/5/2022 02:32:34 PM</span></h4>
+          <h4>Order Id : <span>{order.orderId}</span></h4>
+          <h4>Order Date : <span>{createdAt}</span></h4>
         </div>
         <div className="order-item-section-2">
-          <h4>&#8377; 3600</h4>
+          <h4>&#8377; {order.total}</h4>
         </div>
         <div className="order-item-section-3">
-          <h4>Processing</h4> 
-          <h4>Delivery : <span>30 May 2022</span></h4>
+          <h4>Processing</h4>
+          <h4>Delivery : <span>{delivery}</span></h4>
         </div>
       </div>
     </div>
--- src/Components/Pages/Orders/Orders.js
+++ src/Components/Pages/Orders/Orders.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Orders.css'
 import OrderItem from './OrderItems/OrderItem'
 import Authentication from '../../../Authentication'
+import axios from '../../../axios'
+import { AppContext } from '../../../AppContext'
+import { Link } from 'react-router-dom'
 
 function Orders() {
 
     Authentication()
+    const [orders, setOrders] = useState([])
+    const { loggedIn } = useContext(AppContext)
+
+    useEffect(() => {
+        axios.get('getorder', {
+            headers: { "Authorization": localStorage.getItem('token') }
+        })
+            .then(res => {
+                console.log(res.data)
+                setOrders(res.data)
+            })
+        return () => {
+            setOrders([])
+        }
+    }, [])
 
     return (
         <div>
@@ -13,9 +31,28 @@ function Orders() {
                 <div className="order-section-container">
                     <h1 className='order-section-header'>My Orders</h1>
                     <div className="order-section-items">
-                        <OrderItem />
-                        <OrderItem />
-
+                        {
+                            loggedIn ? (
+                                orders.length > 0 ?
+                                    orders.map((order) =>
+                                        <OrderItem
+                                            key={order._id}
+                                            order={order} />
+                                    ) :
+                                    <div className='order-section-warning shop'>
+                                        You have not ordered anything yet ! <br />
+                                        <Link className='order-section-warning-link shop' to={'/shop'}>
+                                            Shop
+                                        </Link>
+                                    </div>
+                            ) :
+                                <div className='order-section-warning'>
+                                    <Link className='order-section-warning-link' to={'/login'}>
+                                        Login
+                                    </Link>
+                                    to see orders
+                                </div>
+                        }
                     </div>
                 </div>
             </div>

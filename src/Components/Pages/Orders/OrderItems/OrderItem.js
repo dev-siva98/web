154bccae34a2687bc087b1e5f71be373d4db5e80
dev-siva98 <sivaprasadtppba@gmail.com>
2022-07-01 01:46:31 +0530

    order style progress

--- src/Components/Pages/Orders/OrderItems/OrderItem.js
+++ src/Components/Pages/Orders/OrderItems/OrderItem.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './OrderItem.css'
+import OrderDetails from '../OrderModal/OrderDetails'
 
 function OrderItem({ order }) {
+
+  const [show, setShow] = useState(false)
+
   const createdAt = new Date(order.createdAt).toLocaleString('en-IN', { dateStyle: 'medium', timeStyle: 'short' })
   const delivery = new Date(order.delivery).toLocaleDateString('en-IN', { dateStyle: 'medium' })
+  const orderStatus = order.orderStatus
+
+  const handleShow = () => {
+    setShow(!show)
+  }
+
   return (
     <div className='order-item-section'>
-      <div className="order-item-container">
+      {
+        show && <OrderDetails handleShow={handleShow} data={order} />
+      }
+      <div className="order-item-container"
+        onClick={handleShow}
+      >
         <div className="order-item-section-1">
           <h4>Order Id : <span>{order.orderId}</span></h4>
           <h4>Order Date : <span>{createdAt}</span></h4>
@@ -15,7 +30,9 @@ function OrderItem({ order }) {
           <h4>&#8377; {order.total}</h4>
         </div>
         <div className="order-item-section-3">
-          <h4>Processing</h4>
+          <h3 className={orderStatus}>
+            {orderStatus}
+          </h3>
           <h4>Delivery : <span>{delivery}</span></h4>
         </div>
       </div>
---
+++ src/Components/Pages/Orders/OrderModal/OrderDetails.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import './OrderDetails.css'
+import { Backdrop } from '@mui/material'
+import { Close } from '@mui/icons-material'
+
+
+
+
+function OrderDetails({ data, handleShow }) {
+
+    const createdAt = new Date(data.createdAt)
+        .toLocaleString('en-IN',
+            { dateStyle: 'medium', timeStyle: 'short' })
+
+    const delivery = new Date(data.delivery)
+        .toLocaleDateString('en-IN',
+            { dateStyle: 'medium' })
+
+    return (
+        <Backdrop
+            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={true}
+        >
+            <div className='order-details'>
+                <Close
+                    onClick={handleShow}
+                    className='order-details-button' />
+                <div className="order-details-container">
+                    <div className="order-details-item">
+                        <span>Order Id </span><span>{data.orderId}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Order Date</span><span>{createdAt}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>User Name</span><span>{data.userName}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Cart Total</span><span>&#8377; {data.cartTotal}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Coupon Applied</span><span>{data.couponApplied}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Discount</span><span>&#8377; {data.discount}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Shipping</span><span>&#8377; {data.shipping}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Total</span><span>&#8377; {data.total}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Payment Mode</span><span>{data.paymentMode}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Order Status</span><span>{data.orderStatus}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Payment Status</span><span>{data.paymentStatus}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Delivery Date</span><span>{delivery}</span>
+                    </div>
+                    <div className="order-details-item">
+                        <span>Write on Cake</span><span>{data.write}</span>
+                    </div>
+                </div>
+            </div>
+        </Backdrop>
+    )
+}
+
+export default OrderDetails

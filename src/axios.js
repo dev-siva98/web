032c163c9b25e349d93ce2737130a44d881bde79
Sivaprasad <sivaprasadtppba@gmail.com>
2023-10-16 01:56:04 +0530

    backend server url changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL : 'https://makemycake-backend.herokuapp.com'
+    baseURL : 'https://mmc-server.onrender.com'
 })
 
 export default instance;
